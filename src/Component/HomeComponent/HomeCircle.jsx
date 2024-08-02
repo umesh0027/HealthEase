bfe22ea05a3816d2bdbfb3a0fc5bee82a7fc4874
Umesh Yadav <120667524+umesh0027@users.noreply.github.com>
2024-08-02 20:31:52 +0530

    Update HomeCircle.jsx

--- src/Component/HomeComponent/HomeCircle.jsx
+++ src/Component/HomeComponent/HomeCircle.jsx
@@ -1,10 +1,83 @@
+// import React from "react";
+// import CountUp from "react-countup";
+// import "../../assests/CSS/HomeCircle.css";
+
+// const HomeCircles = () => {
+//   return (
+//     <section className=" ccircles">
+//       <div className="circle">
+//         <CountUp
+//           start={0}
+//           end={500}
+//           delay={0}
+//           enableScrollSpy={true}
+//           scrollSpyDelay={500}
+//         >
+//           {({ countUpRef }) => (
+//             <div className="counter">
+//               <span ref={countUpRef} />+
+//             </div>
+//           )}
+//         </CountUp>
+//         <span className="circle-name">
+//           Satisfied
+//           <br />
+//           Patients
+//         </span>
+//       </div>
+//       <div className="circle">
+//         <CountUp
+//           start={0}
+//           end={80}
+//           delay={0}
+//           enableScrollSpy={true}
+//           scrollSpyDelay={500}
+//         >
+//           {({ countUpRef }) => (
+//             <div className="counter">
+//               <span ref={countUpRef} />+
+//             </div>
+//           )}
+//         </CountUp>
+//         <span className="circle-name">
+//           Verified
+//           <br />
+//           Doctors
+//         </span>
+//       </div>
+//       <div className="circle">
+//         <CountUp
+//           start={0}
+//           end={35}
+//           delay={0}
+//           enableScrollSpy={true}
+//           scrollSpyDelay={500}
+//         >
+//           {({ countUpRef }) => (
+//             <div className="counter">
+//               <span ref={countUpRef} />+
+//             </div>
+//           )}
+//         </CountUp>
+//         <span className="circle-name">
+//           Specialist
+//           <br />
+//           Doctors
+//         </span>
+//       </div>
+//     </section>
+//   );
+// };
+
+
+
 import React from "react";
 import CountUp from "react-countup";
 import "../../assests/CSS/HomeCircle.css";
 
 const HomeCircles = () => {
   return (
-    <section className=" ccircles">
+    <section className="ccircles">
       <div className="circle">
         <CountUp
           start={0}
@@ -70,3 +143,6 @@ const HomeCircles = () => {
 };
 
 export default HomeCircles;
+
+
+// export default HomeCircles;
